4d162f2f91fcd5ab2d5a9fb6591334836efe2ced
pulau <muchammad.anas@gmail.com>
2022-06-26 19:43:18 +0700

    14 - Intro React - Tantangan

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,26 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./Contact.css";
 
-const Contact = () => {
+const Contact = (propsData) => {
+    const item = propsData.data
     return (
-        <></>
-    )
-}
+        <>
+            <div className="container">
+                <div className="about-image">
+                       <img src={item.photo} className="img" alt={item.photo} />
+                    <center>
+                    </center>
+                </div>
+                <div className="about-content">
+                    <b><p key={item.name}>{item.name}</p></b>
+                    <p key={item.phone}>{item.phone}</p>
+                    <p key={item.email}>{item.email}</p>
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <>
+        <div className='Header'>
+            <h2>Call a Friend</h2>
+            <p>your friendly contact app</p>
+            <div style={{textAlign: 'center'}}>
+                <hr />
+            </div>
+        </div></>
     )
 }
 
